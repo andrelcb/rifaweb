64bb6f4c930e8d957f2305fa584ab872d7f30f8e
Andre Leonardo <andre.leonardocb@gmail.com>
2022-05-31 03:21:50 -0300

    tela inicial criada, componete de rifaItem criado, tela de login e tela de cadastro criado.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  //resolvendo bug de client server do next e pushando js do bootstrap
+  useEffect(() => { typeof document !== undefined ? require('bootstrap/dist/js/bootstrap') : null }, [])
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
